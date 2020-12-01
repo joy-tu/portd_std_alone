dda9d442193aa5e8097dd33a27fdd86fea547dc0
joy_tu <joy.tu@moxa.com>
2020-11-30 19:14:18 -0800

    1. Add definition of related-platform.

---
+++ include/platform.h
@@ -0,0 +1,12 @@
+/*  Copyright (C) MOXA Inc. All rights reserved.
+
+    This software is distributed under the terms of the
+    MOXA License.  See the file COPYING-MOXA for details.
+*/
+
+#ifndef _PLATFORM_H
+#define _PLATFORM_H
+#define LINUX
+
+#endif /* _PLATFORM_H */
+
